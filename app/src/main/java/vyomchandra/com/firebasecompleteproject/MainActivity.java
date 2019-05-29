26781507c97d0c04d23d73f1223eab636dcef139
vyom chandra gallani <vyomchandra@gmail.com>
2019-05-29 13:24:07 +0530

    added firebase email and password

--- app/src/main/java/vyomchandra/com/firebasecompleteproject/MainActivity.java
+++ app/src/main/java/vyomchandra/com/firebasecompleteproject/MainActivity.java
@@ -1,5 +1,6 @@
 package vyomchandra.com.firebasecompleteproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText email,pass;
     private Button signup,signin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         pass=findViewById(R.id.password_signin);
         signup=findViewById(R.id.sign_up);
         signin=findViewById(R.id.sign_in);
+
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,10 +41,12 @@ public class MainActivity extends AppCompatActivity {
                 String passs=pass.getText().toString().trim();
                 if(TextUtils.isEmpty(emails)){
                     email.setError("required");
+                    return;
 
                 }
                 if(TextUtils.isEmpty(passs)){
                     pass.setError("required");
+                    return;
                 }
             }
         });
--- app/src/main/java/vyomchandra/com/firebasecompleteproject/signup.java
+++ app/src/main/java/vyomchandra/com/firebasecompleteproject/signup.java
@@ -1,17 +1,27 @@
 package vyomchandra.com.firebasecompleteproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
 
     private EditText email,pass;
     private Button signin,signup;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +31,12 @@ public class signup extends AppCompatActivity {
        signin=findViewById(R.id.sign_in);
        email=findViewById(R.id.email_signup);
        pass=findViewById(R.id.password_signup);
-       signin.setOnClickListener(new View.OnClickListener() {
+
+
+        firebaseAuth= FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(this);
+
+        signin.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
                Intent i=new Intent(signup.this,MainActivity.class);
@@ -35,11 +50,29 @@ public class signup extends AppCompatActivity {
                String passs=pass.getText().toString().trim();
                if(TextUtils.isEmpty(emails)){
                    email.setError("required");
+                   return;
 
                }
                if(TextUtils.isEmpty(passs)){
                    pass.setError("required");
+                   return;
                }
+               progressDialog.setMessage("processing...");
+               progressDialog.show();
+               firebaseAuth.createUserWithEmailAndPassword(emails,passs).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                      if(task.isSuccessful()){
+                          Toast.makeText(signup.this, "complete", Toast.LENGTH_SHORT).show();
+                          startActivity(new Intent(signup.this,HomeActivity.class));
+                          progressDialog.dismiss();
+
+                      }else{
+                          Toast.makeText(signup.this, "registration error", Toast.LENGTH_SHORT).show();
+
+                      }
+                   }
+               });
            }
        });
     }

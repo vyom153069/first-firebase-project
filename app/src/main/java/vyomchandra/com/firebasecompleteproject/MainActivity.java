8364338e1430be040827b29a5e144c73ee57d5fe
vyom chandra gallani <vyomchandra@gmail.com>
2019-05-29 18:31:25 +0530

    home page designing

--- app/src/main/java/vyomchandra/com/firebasecompleteproject/MainActivity.java
+++ app/src/main/java/vyomchandra/com/firebasecompleteproject/MainActivity.java
@@ -2,19 +2,27 @@ package vyomchandra.com.firebasecompleteproject;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText email,pass;
     private Button signup,signin;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
 
     @Override
@@ -25,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         pass=findViewById(R.id.password_signin);
         signup=findViewById(R.id.sign_up);
         signin=findViewById(R.id.sign_in);
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(MainActivity.this);
 
 
         signup.setOnClickListener(new View.OnClickListener() {
@@ -42,12 +52,29 @@ public class MainActivity extends AppCompatActivity {
                 if(TextUtils.isEmpty(emails)){
                     email.setError("required");
                     return;
-
                 }
                 if(TextUtils.isEmpty(passs)){
                     pass.setError("required");
                     return;
                 }
+
+                progressDialog.setMessage("prossing...");
+                progressDialog.show();
+                firebaseAuth.signInWithEmailAndPassword(
+
+                        emails,passs).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(task.isSuccessful()){
+                           Toast.makeText(MainActivity.this, "complete", Toast.LENGTH_SHORT).show();
+                           startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                           progressDialog.dismiss();
+                       }else
+                       {
+                           Toast.makeText(MainActivity.this, "Please register first", Toast.LENGTH_SHORT).show();
+                       }
+                    }
+                });
             }
         });
     }

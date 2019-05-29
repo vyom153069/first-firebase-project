2bfc0a90916f1581348846e47d742f1f1e40309d
vyom chandra gallani <vyomchandra@gmail.com>
2019-05-29 19:25:46 +0530

    input data layout designing

--- app/src/main/java/vyomchandra/com/firebasecompleteproject/HomeActivity.java
+++ app/src/main/java/vyomchandra/com/firebasecompleteproject/HomeActivity.java
@@ -1,13 +1,55 @@
 package vyomchandra.com.firebasecompleteproject;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private FloatingActionButton floatingActionButton;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //toolbaar
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView=findViewById(R.id.recyler);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //floating button
+        floatingActionButton=findViewById(R.id.flotingbtn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addData();
+
+            }
+        });
+
+
+    }
+    public void addData(){
+        AlertDialog.Builder mydialoge=new AlertDialog.Builder(this);
+        LayoutInflater inflater=LayoutInflater.from(this);
+        View myview=inflater.inflate(R.layout.inputlayout,null);
+        mydialoge.setView(myview);
+        AlertDialog dialog=mydialoge.create();
+        dialog.show();
+
     }
 }
